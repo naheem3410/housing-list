447b6404488f7b633c54e8e4db1d303fdd518b17
Quadri Naheem <naheemquadri3410@gmail.com>
2024-07-28 03:29:35 +0100

    feat:house UI created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],//[RouterOutlet],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="logo.jpg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,//'./app.component.html'
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-prac';
+  title = 'homes';
 }
